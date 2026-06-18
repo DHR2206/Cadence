03a5d40052da143e218df363ee6697312d428497
Daksh Rathod <rgkx5k4tzr@privaterelay.appleid.com>
2026-06-19 03:03:18 +0530

    Fix 5 changing the route.ts ( continue with google redirectTo URL error)

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,7 +1,14 @@
 import type { NextRequest } from "next/server";
 import { updateSession } from "@/lib/supabase/middleware";
+import { NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+
+  if (pathname.startsWith("/auth/callback")) {
+    return NextResponse.next();
+  }
+
   return updateSession(request);
 }
 
