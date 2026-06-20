4d8d76236d7428d71e6b2dda0f3fc9eaf6ae7e6d
Tanvii13 <tanvinakum79@gmail.com>
2026-06-20 06:12:01 +0530

    Update layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+     <body className="pb-20">
         <AuthStateLogger />
         {children}
         <SiteFooter />
