3cef7065f7a65fa4e42af659bc8838586610e0ce
Tanvii13 <tanvinakum79@gmail.com>
2026-06-20 06:30:19 +0530

    Update layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,21 +1,19 @@
 import { AuthStateLogger } from "@/components/AuthStateLogger";
-import { SiteFooter } from "@/components/SiteFooter";
 import { metadata } from "@/app/metadata";
 import "./globals.css";
 
 export { metadata };
 
 export default function RootLayout({
-  children
+  children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-     <body className="pb-20">
+      <body>
         <AuthStateLogger />
         {children}
-        <SiteFooter />
       </body>
     </html>
   );
